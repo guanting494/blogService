c5e5f271c190695b2d6d683fb63165075bd70f2a
unknown <liuguanting494371725@gmail.com>
2025-05-28 11:09:31 +0900

    update gitignore file

--- backend/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    # 示例接口
-    # path('articles/', article_list_view),
-]
--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-
-urlpatterns = [
-    # OAuth
-    path('', include('dj_rest_auth.urls')), # including login, logout
-    path('signup/', include('dj_rest_auth.registration.urls')),
-    path('social/', include('allauth.socialaccount.urls')),
-]
